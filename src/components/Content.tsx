0bf5d0ead8d082022f9f6e99ff937b17b6cd36a7
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-02-10 18:55:11 -0300

    incremented Content and SideBar components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,26 @@
-export function Content() {
-  // Complete aqui
+import '../styles/content.scss';
+import { ContentHeader } from './ContentHeader';
+import { MoviesListing } from './MoviesListing';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface ContentProps {
+  selectedGenreId: number;
+  selectedGenre: GenreResponseProps;
+}
+
+export function Content({ selectedGenreId, selectedGenre }: ContentProps) {
+
+  return (
+    <>
+      <div className="container">
+        <ContentHeader selectedGenre={selectedGenre}/>
+        <MoviesListing selectedGenreId={selectedGenreId}/>
+      </div>
+    </>
+  )
 }
